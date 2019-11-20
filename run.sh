c6e7e7ce0a374510ebc1bb5373332af7fa6c21e9
joselyn <1326549593@qq.com>
2019-11-20 21:15:28 +0800

    ero init

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+python3.6 main.py --exp_order 0 --EF 5 --q 1
+python3.6 main.py --exp_order 1 --EF 1.2 --q 1.5
+python3.6 main.py --exp_order 2 --EF 0.056 --q 2.5
+#python3.6 main.py --exp_order 3 --EF 22.4 --q 0.5
+#python3.6 main.py --exp_order 4 --EF 30.2 --q 0.4
+
+
 #ero
 python3.6 ero.py --exp_order 0 --percent 0
 python3.6 ero.py --exp_order 1 --percent 0.2
