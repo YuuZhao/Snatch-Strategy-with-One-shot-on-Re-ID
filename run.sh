efd342095c9b9be95300a00d44a3f6ae0f8a0271
joselyn <1326549593@qq.com>
2019-11-21 08:22:33 +0800

    ero init

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-python3.6 main.py --exp_order 0 --EF 5 --q 1
-python3.6 main.py --exp_order 1 --EF 1.2 --q 1.5
-python3.6 main.py --exp_order 2 --EF 0.056 --q 2.5
-#python3.6 main.py --exp_order 3 --EF 22.4 --q 0.5
-#python3.6 main.py --exp_order 4 --EF 30.2 --q 0.4
-
+python3.6 main.py --exp_order 0 --EF 5 --q 1 --dataset mars
+python3.6 main.py --exp_order 1 --EF 1.2 --q 1.5 --dataset mars
+python3.6 main.py --exp_order 2 --EF 0.056 --q 2.5  --dataset mars
+#python3.6 main.py --exp_order 3 --EF 22.4 --q 0.5  --dataset mars
+#python3.6 main.py --exp_order 4 --EF 30.2 --q 0.4  --dataset mars
 
 #ero
 python3.6 ero.py --exp_order 0 --percent 0
