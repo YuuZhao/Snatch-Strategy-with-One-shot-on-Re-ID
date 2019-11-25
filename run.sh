9e1c2d996ef3b39ee80ce28ce26250ab6a1658f5
joselyn <1326549593@qq.com>
2019-11-25 17:21:39 +0800

    add some files

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 #python3.6 nlvm.py --exp_order 0 --percent_vari 1
 #python3.6 nlvm.py --exp_order 1 --percent_vari 0.9
 #python3.6 nlvm.py --exp_order 2 --percent_vari 0.8
-#python3.6 nlvm.py --exp_order 0 --percent_vari 0.8  --exp_name test  --epoch 0
+python3.6 nlvm.py --exp_order 0 --percent_vari 0.8  --exp_name test  --epoch 0
 
 
 
+# experiments_name : NLVM-b2
+#python3.6 nlvm-b2.py --exp_order 0  --exp_name test  --epoch 0  --stop_vari_step 5
+#python3.6 nlvm.py --exp_order 1
+#python3.6 nlvm.py --exp_order 2
